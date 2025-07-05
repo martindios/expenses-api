adb21d8791c546e91692a574910dc2081155a336
martindios <gonzalezdiosmartin@gmail.com>
2025-07-05 17:35:29 +0200

    feat: global handler exception added

---
+++ src/main/java/com/example/jparestful/dto/error/ValidationErrorResponse.java
@@ -0,0 +1,36 @@
+package com.example.jparestful.dto.error;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.validation.FieldError;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ValidationErrorResponse {
+    private String errorCode;
+    private String message;
+    private List<FieldError> fieldErrors;
+    private String path;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    @Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class FieldError {
+        private String field;
+        private Object rejectedValue;
+        private String message;
+    }
+
+}
